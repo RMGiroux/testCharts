0d203ab50f71b689c4466f412ad129bb533bbcb9
Mike Giroux <rmgiroux@gmail.com>
2015-04-10 08:06:10 -0400

    Extract graphing and interpolation logic into a separate module

--- testChart1.py
+++ testChart1.py
@@ -1,207 +1,9 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
 import numpy as np
 import sys
 import os
 import re
 
-# The full list of available colormaps (for testing, not needed at the moment).
-cmaps = ('Blues', 'BuGn', 'BuPu', 'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
-                             'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
-                             'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd',
-         'afmhot', 'autumn', 'bone', 'cool', 'copper',
-                             'gist_heat', 'gray', 'hot', 'pink',
-                             'spring', 'summer', 'winter',
-         'BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
-                             'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
-                             'seismic',
-         'Accent', 'Dark2', 'Paired', 'Pastel1',
-                             'Pastel2', 'Set1', 'Set2', 'Set3',
-         'gist_earth', 'terrain', 'ocean', 'gist_stern',
-                             'brg', 'CMRmap', 'cubehelix',
-                             'gnuplot', 'gnuplot2', 'gist_ncar',
-                             'nipy_spectral', 'jet', 'rainbow',
-                             'gist_rainbow', 'hsv', 'flag', 'prism')
-
-
-
-def linearInterp1DintoTarget(target, source, factor=5):
-    """Interpolates the 'source' vector linearly into the 'target' vector, piece-wise,
-       inserting 'factor' interpolated elements between each of the 'source' elements.
-
-       Examples:
-          source:
-             [ 1., 2., 3. ]
-
-          factor 3:  [ 1., 1.33333333, 1.66666667, 2., 2.33333333, 2.66666667, 3. ]
-
-          factor 5:  [ 1., 1.2, 1.4, 1.6, 1.8, 2., 2.2, 2.4, 2.6, 2.8, 3. ]
-
-    :param target: "Expanded" vector into which interpolated values will be placed
-    :param source: Vector to expand
-    :param factor: Expansion factor
-    :return: np.ndarray
-
-       Requires: 'len(target) == (len(source) + factor * (len(source) - 1))'
-    """
-
-    assert len(target) == (1 + factor * (len(source) - 1))
-
-    for i in range(0, len(source) - 1):
-        tIdx = i * factor
-        delta = (source[i + 1] - source[i]) / (1.0 * factor)
-
-        target[tIdx] = source[i]
-
-        for j in range(0, factor):
-            target[tIdx + j] = source[i] + (j * delta)
-
-    target[-1] = source[-1]
-
-    return target
-
-
-def linearInterp1D(source, factor=5):
-    """Returns a vector expanding on 'source' by inserting 'factor' linearly-interpolated values in between each
-    entry.
-
-    :param source: Vector to be expanded
-    :param factor: Expansion factor
-    :return: np.ndarray
-    """
-
-    if factor == 0:
-        return source
-
-    target = np.zeros(1 + (len(source) - 1) * factor)
-
-    return linearInterp1DintoTarget(target, source, factor)
-
-
-def linearInterp2D(source, factor=5):
-    """Returns a matrix expanding on 'source' by inserting 'factor' linearly-interpolated values
-    in between each entry in both dimensions.
-
-    :param source: Matrix to be expanded
-    :param factor: Expansion factor
-    :return: np.ndarray
-    """
-
-    if factor == 0:
-        return source
-
-    sShape = source.shape
-    rXShape = 1 + factor * (sShape[0] - 1)
-    rYShape = 1 + factor * (sShape[1] - 1)
-    target = np.zeros([rXShape, rYShape])
-
-    for i in range(0, sShape[0]):
-        tXIdx = i * factor
-
-        linearInterp1DintoTarget(target[tXIdx], source[i], factor)
-
-    linearInterp1DintoTarget(target[-1], source[-1], factor)
-
-    for i in range(0, sShape[0] - 1):
-        tXIdx = i * factor
-
-        deltas = np.zeros(rYShape)
-
-        for j in range(0, rYShape):
-            deltas[j] = (target[tXIdx + factor][j] - target[tXIdx][j]) / factor
-
-        for x in range(0, factor):
-            for y in range(0, rYShape):
-                target[tXIdx + x][y] = target[tXIdx][y] + x * deltas[y]
-
-    return target
-
-def plot_graph(X, Y, Z,
-               title,
-               xTitle, xTicks, xLabels,
-               yTitle, yTicks, yLabels,
-               filename,
-               titleFilename,
-               factor=0, block=False, colormap = "jet"):
-    """
-    Plot a 3D surface for X, Y, and Z, interpolating the data by 'factor' if specified.
-
-    If 'filename' is not None, then save the result into 'filename'.  Otherwise, display
-    the graphic interactively, blocking if 'block' is True.
-
-    :param X: the X axis points (1 dimensional np.ndarray)
-    :param Y: the Y axis points (1 dimensional np.ndarray)
-    :param Z: the Z axis points (2 dimensional np.ndarray)
-    :param title: the graph title
-    :param xTitle: the X axis title
-    :param xTicks: the X axis numerical ticks
-    :param xLabels: the X axis labels
-    :param yTitle: the Y axis title
-    :param yTicks: the Y axis numerical ticks
-    :param yLabels: the Y axis labels
-    :param filename: The filename to save into, or, if None, then indicates graph should be displayed
-    :param titleFilename: the filename to save the table graphic into
-    :param factor: Interpolation factor, in order to add polygons to graph and improve appearance without altering shape
-    :param block: Whether or not to block in interactive mode
-    :return: None
-    """
-
-    fig = plt.figure()
-    fig.suptitle(title)
-
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
-
-    ax.set_xlabel(xTitle, fontsize=8)
-    ax.set_xticks(xTicks)
-    ax.set_xticklabels(xLabels)
-
-    ax.set_ylabel(yTitle, fontsize=8)
-    ax.set_yticks(yTicks)
-    ax.set_yticklabels(yLabels)
-
-    ax.tick_params(axis='both', which='major', labelsize=7)
-    ax.tick_params(axis='both', which='minor', labelsize=7)
-
-    Xinterp = linearInterp1D(X, factor)
-    Yinterp = linearInterp1D(Y, factor)
-    Zinterp = linearInterp2D(Z, factor)
-
-    Xinterp, Yinterp = np.meshgrid(Xinterp, Yinterp)
-
-    surf = ax.plot_surface(Xinterp,
-                           Yinterp,
-                           Zinterp,
-                           cmap=plt.get_cmap(colormap),
-                           linewidth=0,
-                           rstride=1, cstride=1,
-                           alpha=1.0
-                           )
-
-    if filename is None:
-        plt.show(block)
-
-        # If block is true, then we we can close the figure(s) after the user is done interacting.
-        if block:
-            plt.close(fig)
-    else:
-        plt.savefig(filename, bbox_inches='tight', pad_inches=0.25)
-        plt.close(fig)
-
-    if titleFilename is not None:
-        hcell, wcell = 0.125, 2.
-        hpad, wpad = 0, 0
-        nrows, ncols = len(yLabels) + 1, len(xLabels)
-        fig=plt.figure(figsize=(ncols*wcell+wpad, nrows*hcell+hpad),dpi=600)
-        ax=fig.add_subplot('111')
-        ax.axis('off')
-        ax.table(cellText=Z,
-                 rowLabels=yLabels,
-                 colLabels=xLabels,
-                 loc='center')
-        plt.savefig(titleFilename, bbox_inches='tight')
-        plt.close(fig)
-
+import graphSupport
 
 # http://stackoverflow.com/questions/635483/what-is-the-best-way-to-implement-nested-dictionaries-in-python/19829714#19829714
 class Vividict(dict):
@@ -231,8 +33,8 @@ with open("processed_test_data.out") as f:
                 = fields[3]
 
 for dataset in values.keys():
-    # if dataset != 25:
-    #     continue
+    if dataset != 25:
+        continue
 
     X = range(0, 9)
     Y = values[dataset].keys()
@@ -250,13 +52,17 @@ for dataset in values.keys():
     for factorArg in (15, ):
         for cmap in ('jet', ):
             print "Plotting dataset %d at factor %d (colormap %s)" % (dataset, factorArg, cmap)
-            plot_graph(X, Y, mat, "Problem Size = $2^{%d}$" % dataset,
-                       "Temporal Locality",
-                       [0, 1, 2, 3, 4, 5, 6, 7, 8],
-                       ['$2^{-%d}$' % i for i in range(0, 9)],
-                       "Subsystem Size",
-                       Y,
-                       ['$2^{%d}$' % int(i) for i in Y],
-                       "images/foo_%02d_%d_%s.png" % (dataset, factorArg, cmap),
-                       "images/foo_%02d_table.png" % dataset,
-                       factor=factorArg, colormap=cmap)
+            graphSupport.output_plot_and_table(X,
+                                               Y,
+                                               mat,
+                                               "Problem Size = $2^{%d}$" % dataset,
+                                               "Temporal Locality",
+                                               [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                                               ['$2^{-%d}$' % i for i in range(0, 9)],
+                                               "Subsystem Size",
+                                               Y,
+                                               ['$2^{%d}$' % int(i) for i in Y],
+                                               "images/foo_%02d_%d_%s.png" % (dataset, factorArg, cmap),
+                                               "images/foo_%02d_table.png" % dataset,
+                                               factor=factorArg,
+                                               colormap=cmap)
